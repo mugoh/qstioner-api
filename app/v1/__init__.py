9647e4fc2dad9cf094b0981fabf92a9af1dbe159
root <mugoh.ks@gmail.com>
2019-01-08 22:36:21 +0300

    [Feature #163048124] Access auth blueprint at app instance

--- app/v1/__init__.py
+++ app/v1/__init__.py
@@ -1,10 +1,10 @@
 from flask_restful import Api
 from flask import Blueprint
 
-from views.user import UsersList
+from app.v1.views.user import UsersRegistration
 
 auth_blueprint = Blueprint('auth', __name__, url_prefix='/api/v1/auth/')
 
 auth_api = Api(auth_blueprint)
 
-auth_api.add_resource(UsersList, '/register')
+auth_api.add_resource(UsersRegistration, '/register')
