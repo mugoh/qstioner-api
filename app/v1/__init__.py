36f1a978a1a2fb248ecd8f56df25ac17c7bed826
root <mugoh.ks@gmail.com>
2019-01-09 01:08:10 +0300

    [Finishes #163070993] Add an endpoint for the logout resource

--- app/v1/__init__.py
+++ app/v1/__init__.py
@@ -1,7 +1,7 @@
 from flask_restful import Api
 from flask import Blueprint
 
-from app.v1.views.user import UsersRegistration, UserLogin
+from app.v1.views.user import UsersRegistration, UserLogin, UserLogout
 
 auth_blueprint = Blueprint("auth", __name__, url_prefix='/api/v1/auth/')
 
@@ -9,3 +9,4 @@ auth_api = Api(auth_blueprint)
 
 auth_api.add_resource(UsersRegistration, 'register')
 auth_api.add_resource(UserLogin, 'login')
+auth_api.add_resource(UserLogout, 'logout')
