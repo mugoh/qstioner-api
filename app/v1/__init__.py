b695c8938ae88602b2475c4848f6b9278d524bcc
root <mugoh.ks@gmail.com>
2019-01-09 00:16:35 +0300

    [Feature #163070948] Specify login url

--- app/v1/__init__.py
+++ app/v1/__init__.py
@@ -1,10 +1,11 @@
 from flask_restful import Api
 from flask import Blueprint
 
-from app.v1.views.user import UsersRegistration
+from app.v1.views.user import UsersRegistration, UserLogin
 
 auth_blueprint = Blueprint("auth", __name__, url_prefix='/api/v1/auth/')
 
 auth_api = Api(auth_blueprint)
 
 auth_api.add_resource(UsersRegistration, 'register')
+auth_api.add_resource(UserLogin, 'login')
