c5847e88361bc415aea6a28e243db63be59ad4bd
root <mugoh.ks@gmail.com>
2019-01-08 23:49:36 +0300

    [Starts #163070948] Bundle up all post data errors and present them to user together

--- instance/config.py
+++ instance/config.py
@@ -4,13 +4,10 @@ import os
 class BaseConfig:
     DEBUG = False
     TESTING = False
-    SECRET_KEY = os.environ.get('SECRET_KEY',
-                                b'\xc2;F]l\x046t\xfe\x08'
-                                )
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     CSRF_ENABLED = True
-    JWT_SECRET_KET = os.environ.get('JWT_SECRET_KEY',
-                                    b'\xa3$\x0bm\xae\xbbmd\x12\x8f'
-                                    )
+    BUNDLE_ERRORS = True
+    JWT_SECRET_KET = os.environ.get('JWT_SECRET_KEY')
 
 
 class DevelopmentConfig(BaseConfig):
