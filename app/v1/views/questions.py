2f7c7bdb654c344632b435e9c989835212ab59c8
root <mugoh.ks@gmail.com>
2019-01-09 22:32:47 +0300

    [Feature #163091026] Show the user status codes on a response

--- app/v1/views/questions.py
+++ app/v1/views/questions.py
@@ -22,3 +22,29 @@ class Questions(Resource):
         parser.add_argument('body', type=str, required=True)
 
         args = parser.parse_args(strict=True)
+
+        new_questn = QuestionModel(**args)
+
+        if not QuestionModel.verify_existence(new_questn):
+            new_questn.save()
+
+        else:
+            return {
+                "Status": 409,
+                "Message": "Chill up. That question is already created"
+            }, 409
+
+        return {
+            "Status": 201,
+            "Data": [new_questn.dictify()]
+        }, 201
+
+    def get(self):
+        """
+            Returns all exsisting questions
+        """
+
+        return {
+            "Status": 200,
+            "Data": [QuestionModel.get_all_questions()]
+        }, 200
