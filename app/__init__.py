a25a22e0193dab21bb9af1f4a8e05b55b8b93dd5
root <mugoh.ks@gmail.com>
2019-01-09 09:36:12 +0300

    [Feature #163079292] Register application blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
 
-from app.v1 import auth_blueprint
+from app.v1 import auth_blueprint, app_blueprint
 from app.v1.views.user import blacklisted_tokens
 from instance.config import APP_CONFIG
 
@@ -9,6 +9,7 @@ from instance.config import APP_CONFIG
 def create_app(config_setting):
     app = Flask(__name__)
     app.register_blueprint(auth_blueprint)
+    app.register_blueprint(app_blueprint)
 
     jwt = JWTManager(app)
 
