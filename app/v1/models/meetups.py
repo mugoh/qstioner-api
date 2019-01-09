09949807e5b7daec9f81be33fbaddeee8b25ea87
root <mugoh.ks@gmail.com>
2019-01-09 08:08:39 +0300

    [Feature #163079292] Specify required parameters

--- app/v1/models/meetups.py
+++ app/v1/models/meetups.py
@@ -4,7 +4,16 @@
 
 from app.v1.models.abstract_model import AbstractModel
 
+meetups = []  # Holds all meetups records
+
 
 class MeetUpModel(AbstractModel):
 
-    def __init__(**kargs):
+    def __init__(self, **kargs):
+
+        super().__init__(meetups)
+        self.location = kargs.get('location')
+        self.images = kargs.get('images')
+        self.topic = kargs.get('topic')
+        self.happeningOn = kargs.get('happeningOn')
+        self.tags = kargs.get('tags')
