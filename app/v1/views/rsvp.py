a1ecc167d7c170ed6162990fcd036d532422889c
root <mugoh.ks@gmail.com>
2019-01-10 22:57:09 +0300

    [Feature #163112413] Pass user arguments to create new rsvp

--- app/v1/views/rsvp.py
+++ app/v1/views/rsvp.py
@@ -4,10 +4,11 @@
 """
 
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 from app.v1.models.rsvp import RsvpModel
 from ..models.meetups import MeetUpModel
+from ..models.users import UserModel
 
 
 class Rsvps(Resource):
@@ -27,4 +28,28 @@ class Rsvps(Resource):
         # Confirm existence of meetup
 
         if not MeetUpModel.get_by_id(id):
-            return
+            return {
+                "Status": 400,
+                "Message": "That meetup does not exist"
+            }, 400
+
+        user = UserModel.get_by_name(get_jwt_identity()).id
+
+        args.update({
+            "user": user
+        })
+
+        # Create rsvp and confirm it's not a duplicate
+
+        rsvp = RsvpModel(**args)
+        if not RsvpModel.verify_unique(rsvp):
+            return {
+                "Status": 409,
+                "Message": "You've done that same rsvp already"
+            }, 409
+        rsvp.save()
+
+        return {
+            "Status": 201,
+            "Data": rsvp.dictify()
+        }, 201
