580283fd3e616a3effac641a3690ddfba2c6945d
root <mugoh.ks@gmail.com>
2019-01-10 10:16:25 +0300

    [Chore #163108185] Test for missing question resources and votes

--- tests/v1/question_test.py
+++ tests/v1/question_test.py
@@ -32,15 +32,65 @@ class TestQuestions(BaseTestCase):
         self.assertEqual(response.status_code, 200,
                          msg="Fails to fetch individual question")
 
+    def test_get_non_existent_question(self):
+
+        response = self.client.get('api/v1/questions/400',
+                                   content_type='application/json',
+                                   headers=self.auth_header)
+        self.assertEqual(response.status_code, 404,
+                         msg="Fails to return error\
+                         on fetching missing question")
+
     def test_create_existing_question(self):
-        self.new_question = json.dumps(dict(
-            title="One Question",
-            body="This looks lika a body"))
+        new_question = json.dumps(dict(
+            title="One Question Dup",
+            body="This looks like a body"))
+
+        self.client.post('api/v1/questions',
+                         data=new_question,
+                         content_type='application/json',
+                         headers=self.auth_header)
 
         response = self.client.post('api/v1/questions',
-                                    data=self.new_question,
+                                    data=new_question,
                                     content_type='application/json',
                                     headers=self.auth_header)
-        self.assertEqual(response.status_code, 201,
+        self.assertEqual(response.status_code, 409,
                          msg="Fails to avoid creation\
                          of question with same data twice")
+
+    def test_down_vote_question(self):
+        res = self.client.patch('api/v1/questions/1/downvote',
+                                content_type='application/json',
+                                headers=self.auth_header)
+
+        expected_votes = res.get_json().get('Data')[0].get('votes')
+
+        self.assertEqual(expected_votes, -1,
+                         msg="Fails to downvote a question")
+
+    def test_up_vote_question(self):
+        res = self.client.patch('api/v1/questions/1/upvote',
+                                content_type='application/json',
+                                headers=self.auth_header)
+
+        expected_votes = res.get_json().get('Data')[0].get('votes')
+
+        self.assertEqual(expected_votes, 0,
+                         msg="Fails to upvote a question")
+
+    def test_vote_non_existent_question(self):
+        res = self.client.patch('api/v1/questions/500/upvote',
+                                content_type='application/json',
+                                headers=self.auth_header)
+
+        self.assertEqual(res.status_code, 404,
+                         msg="Fails. Votes a missing question")
+
+    def test_vote_question_with_invalid_string(self):
+        res = self.client.patch('api/v1/questions/1/invalid',
+                                content_type='application/json',
+                                headers=self.auth_header)
+
+        self.assertEqual(res.status_code, 400,
+                         msg="Fails to validate vote request")
