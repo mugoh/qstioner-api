fd3e90c676cf7398f6f8c925ea5d7856746f83ee
root <mugoh.ks@gmail.com>
2019-01-10 09:44:16 +0300

    [Chore #163108185] Check for status code on post question request

--- tests/v1/question_test.py
+++ tests/v1/question_test.py
@@ -9,8 +9,9 @@ class TestQuestions(BaseTestCase):
             title="One Question",
             body="This looks lika a body"))
 
-        response = self.client.post('api/v1/questions/',
+        response = self.client.post('api/v1/questions',
                                     data=self.new_question,
                                     content_type='application/json',
                                     headers=self.auth_header)
-        self.assertEqual(response.status_code, 203)
+        print(response.data.decode())
+        self.assertEqual(response.status_code, 201)
