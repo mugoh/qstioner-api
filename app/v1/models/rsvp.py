3f4308fa12d73573969edac25920d157da1bb706
root <mugoh.ks@gmail.com>
2019-01-10 12:35:48 +0300

    [Feature #163112413] Implement rsvp search behaviours

--- app/v1/models/rsvp.py
+++ app/v1/models/rsvp.py
@@ -6,7 +6,50 @@ rsvps = []
 class RsvpModel(AbstractModel):
 
     def __init__(self, **kwargs):
+
         super().__init__(rsvps)
         self.user = kwargs['user']
         self.meetup = kwargs['meetup']
         self.response = kwargs['response']
+
+    def save(self):
+        """
+            Saves rsvp instance to the present record
+            holding all rsvps
+        """
+        rsvps.append(self)
+
+    def dictify(self):
+        """
+            Returns a dictionary of the rsvp instance
+            for readability of the rsvp's fields.
+        """
+
+        return {
+            "response": self.response,
+            "meetup": self.meetup,
+            "user": self.user,
+        }
+
+        #
+        # Searches
+
+    @classmethod
+    def get_all_rsvps(cls):
+        """
+            Converts all present rsvp objects to a
+            dictionary and sends them in a list envelope
+        """
+        return [rsvp.dictify() for rsvp in rsvps]
+
+    @classmethod
+    def verify_unique(cls, rsvp_object):
+        """
+            Helps in ensyring a user does not rsvp for
+            the meetup twice with the same rsvp data.
+        """
+        return any([rsvp for rsvp in rsvps
+                    if repr(rsvp) == repr(rsvp_object)])
+
+    def __repr__(self):
+        return '{response} {meetup} {user}'.format(**self.dictify())
