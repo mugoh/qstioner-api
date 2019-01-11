27ab734f6a85de871b16d08ccaa896db384793e7
root <mugoh.ks@gmail.com>
2019-01-11 23:03:52 +0300

    [Feature #163154676] Register new resource endpoints

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -4,7 +4,7 @@ from flask import Blueprint
 from ...api.v1.views.user import UsersRegistration, UserLogin, UserLogout
 from ...api.v1.views.meetups import Meetups, MeetUp, MeetUpItem
 from ...api.v1.views.questions import Question, Questions, QuestionVote
-from ...api.v1.views.rsvp import Rsvps
+from ...api.v1.views.rsvp import Rsvps, Rsvp
 
 auth_blueprint = Blueprint("auth", __name__, url_prefix='/api/v1/auth/')
 app_blueprint = Blueprint("app", __name__, url_prefix='/api/v1/')
@@ -24,3 +24,4 @@ app_api.add_resource(Question, 'questions/<int:id>')
 app_api.add_resource(QuestionVote, 'questions/<int:id>/<vote>',
                      methods=['PATCH'])
 app_api.add_resource(Rsvps, 'meetups/<int:id>/<response>')
+app_api.add_resource(Rsvp, 'meetups/<int:id>/rsvp', 'meetups/<username>/rsvp')
