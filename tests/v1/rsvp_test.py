ccbadf4229ada9d2ae286c007df9e83a28e50136
root <mugoh.ks@gmail.com>
2019-01-11 08:45:31 +0300

    [Chore #163140447] Use post and get from super

--- tests/v1/rsvp_test.py
+++ tests/v1/rsvp_test.py
@@ -4,18 +4,16 @@ from .base_test import BaseTestCase
 class RSVPTest(BaseTestCase):
 
     def test_create_new_rsvp(self):
-        response = self.client.post('api/v1/meetups/1/yes',
-                                    content_type='application/json',
-                                    headers=self.auth_header)
+
+        response = self.post('api/v1/meetups/1/yes')
+
         self.assertEqual(response.status_code, 201,
                          msg="Fails to rsvp for a meetup")
 
     def test_create_rsvp_with_unknown_response(self):
         # Known responses are 'yes', 'no' and 'maybe'
 
-        response = self.client.post('api/v1/meetups/1/so',
-                                    content_type='application/json',
-                                    headers=self.auth_header)
+        response = self.get('api/v1/meetups')
         self.assertEqual(response.status_code, 400,
                          msg="Fails to not\
                          create an rsvp with an invalid response")
