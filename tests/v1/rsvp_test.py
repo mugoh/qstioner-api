b9752a637bde488a8f11673b40938c318a26c9b5
root <mugoh.ks@gmail.com>
2019-01-11 08:47:32 +0300

    [Chore #163140447] Perform Post request for rsvp

--- tests/v1/rsvp_test.py
+++ tests/v1/rsvp_test.py
@@ -13,7 +13,7 @@ class RSVPTest(BaseTestCase):
     def test_create_rsvp_with_unknown_response(self):
         # Known responses are 'yes', 'no' and 'maybe'
 
-        response = self.get('api/v1/meetups')
+        response = self.post('api/v1/meetups/1/so')
         self.assertEqual(response.status_code, 400,
                          msg="Fails to not\
                          create an rsvp with an invalid response")
