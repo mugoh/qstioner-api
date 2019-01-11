3f906901dd72464825a79f739d0d54b9cde05967
root <mugoh.ks@gmail.com>
2019-01-11 11:42:41 +0300

    [Chore #163037644] Edit meetup views to reflect changed app structure

--- app/api/v1/views/meetups.py
+++ app/api/v1/views/meetups.py
@@ -2,8 +2,8 @@ from flask_restful import Resource, reqparse
 from flask_jwt_extended import jwt_required, get_jwt_identity
 import datetime
 
-from ...models.meetups import MeetUpModel
-from ...utils.helpers import admin_required
+from ..models.meetups import MeetUpModel
+from ..utils.helpers import admin_required
 
 
 class Meetups(Resource):
