d5b1c54f9691db8de427d5dc70572188fb6f1530
root <mugoh.ks@gmail.com>
2019-01-12 20:40:31 +0300

    [Chore #163165543] Re-edit missing str header details in post request

--- app/tests/v1/auth_test.py
+++ app/tests/v1/auth_test.py
@@ -72,6 +72,34 @@ class AuthTestCases(BaseTestCase):
                         msg="Fails. Registers user with password\
                         of length less than six characters")
 
+    def test_register_with_invalid_usrname(self):
+        user_data = json.dumps(dict(
+            username="Domesticable Cow",
+            email="cow@mammals.milkable",
+            password="pass"))
+
+        response = self.client.post('/api/v1/auth/register',
+                                    data=user_data,
+                                    content_type='application/json')
+
+        self.assertTrue(response.status_code == 400,
+                        msg="Fails. Allows user \
+                        to register with invalid user name")
+
+    def test_register_with_digits_usrname(self):
+        user_data = json.dumps(dict(
+            username="Domesticable45Cow",
+            email="cow@mammals.milkable",
+            password="pass"))
+
+        response = self.client.post('/api/v1/auth/register',
+                                    data=user_data,
+                                    content_type='application/json')
+
+        self.assertTrue(response.status_code == 400,
+                        msg="Fails. Allows user \
+                        to register with invalid user name")
+
     def test_send_request_with_invalid_json(self):
         response = self.client.post('/api/v1/auth/register',
                                     data=self.user_data,
