83ada975a49b2de6434069a53b95a18367b21f5a
root <mugoh.ks@gmail.com>
2019-01-12 13:20:19 +0300

    [Chore #163165543] Store Blacklisted tokens as objects

---
+++ app/api/v1/models/tokens.py
@@ -0,0 +1,22 @@
+"""
+    This module contains the class that creates blacklisted token objects
+    and checks for token validity
+"""
+
+
+class Tokens:
+
+    def __init__(self, token):
+        self.signature = token
+        self.save(token)
+
+    def save(self):
+        blacklisted_tokens.append(self)
+
+    @classmethod
+    def check_if_blacklisted(cls, token):
+        return any([token for token in blacklisted_tokens if
+                    getattr(token, 'signature') == token])
+
+
+blacklisted_tokens = set()
