b230778bf9f9407a92613fef5051563c87b35c5d
root <mugoh.ks@gmail.com>
2019-01-13 08:09:34 +0300

    [Chore #163175325] Configure app documentation

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+from flasgger import Swagger
 
 from app.api.v1 import auth_blueprint, app_blueprint
 from instance.config import APP_CONFIG
@@ -14,5 +15,17 @@ def create_app(config_setting):
         APP_CONFIG[config_setting.strip().lower()])
 
     CORS(app)
+
+    template = {
+        "swagger": "3.0",
+        "info": {
+            "title": "Questioner API",
+            "description": "API for the Questioner \
+            Application with data structures for persistence",
+            "version": "1.0"
+        }
+    }
+
+    Swagger(app, template=template)
 
     return app
